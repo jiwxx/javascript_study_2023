451f4efb0e9a293ea19b4c1e95748b3699135da6
jiwoo <wldnslzhf@gmail.com>
2023-07-28 11:54:43 +0900

    data-더하기빼기 연산연습

--- script/day03_event.js
+++ script/day03_event.js
@@ -8,17 +8,14 @@ console.log (input_id); //정상확인
 //2.이벤트 작성
 //btn3 클릭했을 때 경고창 띄우기
 //객체.이벤트리스너('이벤트 종류',함수(){실행결과})
-btn3.addEventListener('click',function(){
-    window.alert('error');});
-btn4.addEventListener('mouseover',function(){
-    this.style.backgroundColor = 'aqua';});
+btn3.addEventListener('click',function(){window.alert('error');});
+btn4.addEventListener('mouseover',function(){this.style.backgroundColor = 'aqua';});
 //btn4(변수).addEventListener('mouseover(이벤트)',function(실행),(){결과})
 
 btn5.addEventListener('click',function(){
     btn4.style.transform = 'skew(10deg)';
     btn3.style.backgroundColor = 'blue';
-    btn2.innerHTML = 'dd';
-});
+    btn2.innerHTML = 'dd';});
 input_id.addEventListener('focus',function(){this.style.backgroundColor = 'lime'})
 input_id.addEventListener('blur',function(){this.style.backgroundColor = 'skyblue'})
 //변수.addEventListener('이벤트',function(){실행});
